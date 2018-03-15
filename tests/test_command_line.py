6ae7f19a011ac657aee0f586e0d0b533f113d3c0
Darien Hager <darien+git@technofovea.com>
2018-03-14 18:07:23 -0700

    Change most exception-checking to use context-manager format instead

--- tests/test_command_line.py
+++ tests/test_command_line.py
@@ -10,55 +10,54 @@ class TestArgumentParsing(unittest.TestCase):
         self.parser = create_parser()
 
     def test_help(self):
-        try:
+        cmd = ["-h"]
+        with self.assertRaises(SystemExit) as ecm:
             with captured_output() as (out, err):
-                self.parser.parse_args(["-h"])
-            self.fail("Expected to end")
-        except SystemExit as e:
-            self.assertEqual(e.code, 0)
+                self.parser.parse_args(cmd)
+                self.fail("Expected to end")
+        self.assertEqual(ecm.exception.code, 0)
 
     def test_action_required(self):
-        try:
+        cmd = ["--key", "mykey"]
+        with self.assertRaises(SystemExit) as ecm:
             with captured_output() as (out, err):
-                self.parser.parse_args(["--key", "mykey"])
-            self.fail("Expected to end")
-        except SystemExit as e:
-            self.assertEqual(e.code, 2)
+                self.parser.parse_args(cmd)
+                self.fail("Expected to end")
+        self.assertEqual(ecm.exception.code, 2)
 
     def test_symbols(self):
-        try:
-            cmdline = ["symbols", "--key", "mykey"]
-            with captured_output() as (out, err):
-                args = self.parser.parse_args(cmdline)
-            self.assertIsNotNone(args)
-        except SystemExit as e:
-            self.fail("Did not expect exit attempt with code %d" % e.code)
+        cmdline = ["symbols", "--key", "mykey"]
+        with captured_output() as (out, err):
+            args = self.parser.parse_args(cmdline)
+        self.assertIsNotNone(args)
 
     def test_stats_basic(self):
-        try:
-            cmdline = ["stats", "--key", "mykey", "2001-01", "2020-01", "BUY", "N", "LARGE"]
-            with captured_output() as (out, err):
-                args = self.parser.parse_args(cmdline)
-            self.assertIsNotNone(args)
-        except SystemExit as e:
-            self.fail("Did not expect exit attempt with code %d" % e.code)
+        cmdline = ["stats", "--key", "mykey",
+                   "2001-01", "2020-01",
+                   "BUY", "N", "LARGE"
+                   ]
+        with captured_output() as (out, err):
+            args = self.parser.parse_args(cmdline)
+        self.assertIsNotNone(args)
 
     def test_missing_key(self):
-        try:
+        with self.assertRaises(SystemExit) as ecm:
             with captured_output() as (out, err):
                 self.parser.parse_args(["symbols"])
             self.fail("Expected to end with error")
-        except SystemExit as e:
-            self.assertEqual(e.code, 2)
+        self.assertEqual(ecm.exception.code, 2)
 
     def test_bad_date(self):
-        try:
-            cmdline = ["stats", "--key", "mykey", "2001-15", "2020-99", "BUY", "N", "LARGE"]
+        with self.assertRaises(SystemExit) as ecm:
+            cmdline = ["stats", "--key", "mykey",
+                       "2001-15", "2020-99",
+                       "BUY", "N", "LARGE"
+                       ]
             with captured_output() as (out, err):
                 args = self.parser.parse_args(cmdline)
             self.fail("Expected to end with error")
-        except SystemExit as e:
-            self.assertEqual(e.code, 2)
+        self.assertEqual(ecm.exception.code, 2)
+
 
 if __name__ == '__main__':
     unittest.main()
